2314273187a14cfbe7dcd5a8f928ea3519771499
Geoff Nelson <geoff.nelson116@gmail.com>
2020-12-24 09:42:49 -0600

    Got double-conssonant rule done for breaking words to syllables

--- src/BreakWordToSyllables.py
+++ src/BreakWordToSyllables.py
@@ -1,27 +1,60 @@
-
-def BreakWordToSyllables(word):
-    # List of vowels
-    vowels = ['a', 'e', 'i', 'o', 'u', 'y' ]
-
+class WordToSyllable:
     # List of single sound exceptions for double consonants
     single_sound_pairs = ['th', 'sh', 'ph', 'ch', 'wh']
+    # English vowels
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y' ]
+    # Used to keep track of last syllable break
+    last_break=0
+    # Lists to hold syllables
+    syllables = []
 
-    # Iterate over the string, character by character
-    num_vowels=0
-    for i, v in enumerate(word):
-        # If at end of the word, nothing to do
-        if i == len(word)-1:
-            break
-        # Determine if hit a vowel
-        if v in vowels:
-            num_vowels+=1
-        else: # Must be a consonant.
-        # First check for double consonant, indicating a break
+    def __init__(self,word):
+        self.word = word
+        # Iterate over the word, character by character
+        char_types=[]
+        for i,v in enumerate(self.word):
+            char_types.append(self.VowelCheck(v))
 
-            print(word[i+1])
+            #
+            # Check rules for syllables
+            #
 
+            # Cannot check rules on first character
+            if i == 0:
+                continue
 
+            # If two consecutive consonants
+            if char_types[i] == 'c' and char_types[i-1] == 'c':
 
+                # Handle exception of if two consonants make a single sound
+                char_pair = word[i-1] + word[i]
+                if char_pair not in self.single_sound_pairs:
+                    if i <= self.last_break+1:
+                        continue
+                    self.BreakHandler(i)
+                    self.last_break = i
+                else:
+                    if len(self.syllables) != 0:
+                        self.syllables[-1]+=char_pair
+                        self.last_break+=2
 
+        # End of rules, add remaining part of word to last_syllables
+        self.BreakHandler(i)
 
-BreakWordToSyllables('test')
+    def VowelCheck(self,char):
+        # List of vowels
+        if char in self.vowels:
+            return 'v'
+        else:
+            return 'c'
+
+    def BreakHandler(self,char_idx):
+        if char_idx == len(self.word)-1:
+            # If at the end, at the remaining characters to the syllables list
+            self.syllables.append(self.word[self.last_break:])
+        else:
+            # If a break rule was hit, add the characters to the left as a syllable
+            self.syllables.append(self.word[self.last_break:char_idx])
+
+test_tool = WordToSyllable('birthday')
+print(test_tool.syllables)

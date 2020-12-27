d253b8d47a2f90660e77c241ccfa42cac5ce022b
Geoff Nelson <geoff.nelson116@gmail.com>
2020-12-27 13:38:33 -0600

    re-work file path construction. Also add function to read all file lists in a directory

--- src/FileIO.py
+++ src/FileIO.py
@@ -1,33 +1,50 @@
 from pathlib import Path
+import os, os.path
 
 # Handles contructing path from catagory
-def PathFromCatagory(filename,catagory):
-    if catagory == 'names' or catagory == 'name':
-        path = Path(__file__).parent / "../names/"
-    elif catagory == 'trait' or catagory == 'traits':
-        path = Path(__file__).parent / "../traits/"
-    elif catagory == 'jobs' or catagory == 'job':
-        path = Path(__file__).parent / "../jobs/"
-    else:
-        print('WARNING! No defined extra pathing given for catagory!')
-        path = Path(__file__).parent / ".."
-    path = path / str(filename)
+def PathContructor(filename=None,catagory=None):
+    # Step back from src directory
+    path = Path(__file__).parent / ".."
+
+    # If catagory, step path into that catagory
+    if catagory != None:
+        if catagory == 'names' or catagory == 'name':
+            path = Path(__file__).parent / "../names/"
+        elif catagory == 'trait' or catagory == 'traits':
+            path = Path(__file__).parent / "../traits/"
+        elif catagory == 'jobs' or catagory == 'job':
+            path = Path(__file__).parent / "../jobs/"
+
+    # Add filename if provided
+    if filename != None:
+        path = path / str(filename)
+
     return path
 
 # Handles reading list from file
 def ReadListFromFile(filename,catagory):
     # Construct a relative path structure
-    path = PathFromCatagory(filename,catagory)
+    path = PathContructor(filename,catagory)
 
     # Read each name in file into a list
     with open(path, 'r') as f:
         file_list = f.read().splitlines()
     return file_list
 
+def ReadAllFromDirectory(catagory):
+    path = PathContructor()
+    all_elem = []
+    for name in os.listdir(path):
+        if os.path.isfile(name):
+            with open(os.path.join(DIR, name),'r') as filehandle:
+                all_elem.append(filehandle.read().splitlines())
+
+    return all_elem
+
 # Handles writing contents of a list to file
 def WriteListToFile(my_list,filename,catagory):
     # Construct a relative path structure
-    path = PathFromCatagory(filename,catagory)
+    path = PathContructor(filename,catagory)
 
     # Write contents to file
     with open(path, 'w') as filehandle:
